7ac9f7fb6a6a0bc11813ca0f0d73ecdd00f56b2f
Evan Tyler <evantyler6767@gmail.com>
2024-03-02 17:15:01 -0500

    Retriever class added to make api requests

--- Retriever.py
+++ Retriever.py
@@ -1,9 +1,18 @@
-import requests
+import requests as r
 class Retriever:
-    
-    def __init__(self, URL):
+
+
+    CITY = "London"
+    def __init__(self, URL, KEY):
         self.URL = URL
-    
+        self.KEY = KEY
+
+    def send(self):
+        sendurl = self.URL + "q=" + self.CITY+ "&appid=" + self.KEY
+        print(sendurl)
+        response = r.get(sendurl).json()
+        print(response)
+
     def print(self):
 
         print(self.URL)
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ class main:
 
 
     def main():
-        r1 = Retriever.Retriever("frank.com")
+        r1 = Retriever.Retriever("https://api.openweathermap.org/data/2.5/weather?","d252374e5abf5d1d0c777e96c85263c1")
+        r1.send()
         r1.print()
 
     if __name__ == '__main__':
