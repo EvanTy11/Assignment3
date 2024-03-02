e654dd9cd6f410602cba2e05e4a89facf0173199
Evan Tyler <evantyler6767@gmail.com>
2024-03-02 16:12:06 -0500

    new file:   Retriever.py 	modified:   main.py

---
+++ Retriever.py
@@ -0,0 +1,10 @@
+import requests
+class Retriever:
+    
+    def __init__(self, URL):
+        self.URL = URL
+    
+    def print(self):
+
+        print(self.URL)
+     #def getJSON():
---
+++ main.py
@@ -0,0 +1,10 @@
+import Retriever
+class main:
+
+
+    def main():
+        r1 = Retriever.Retriever("frank.com")
+        r1.print()
+
+    if __name__ == '__main__':
+        main()
